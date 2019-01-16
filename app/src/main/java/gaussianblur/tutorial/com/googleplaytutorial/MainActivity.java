87d65daff4d3ef7c604c2e04b22d93b8196e2ee6
lorence <lorence@enclave.vn>
2019-01-16 10:54:27 +0700

    Init

---
+++ app/src/main/java/gaussianblur/tutorial/com/googleplaytutorial/MainActivity.java
@@ -0,0 +1,15 @@
+package gaussianblur.tutorial.com.googleplaytutorial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    // https://developers.google.com/pay/api/android/samples/quickstart
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
